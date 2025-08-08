32341ba03bc96cce62606fd7d267cbf4669c581a
lancemethot <lance.methot@gmail.com>
2025-08-08 06:06:37 -0400

    day 5 part 2 slow

--- day5/day5.test.ts
+++ day5/day5.test.ts
@@ -20,12 +20,29 @@ function determinePassword(doorId: string): string {
     return password;
 }
 
+function determineEnhancedPassword(doorId: string): string {
+    let i = 0;
+    let password: string[] = Array(8).fill('_');
+    while(true) {
+        const hash = md5(`${doorId}${i}`);
+        if(hash.startsWith('00000') && hash.length > 6) {
+            let position = Number.parseInt(hash[5]);
+            if(Number.isInteger(position) && position < password.length && password[position] === '_') {
+                password[position] = hash[6];
+                if(password.every(chr => chr !== '_')) break;
+            }
+        }
+        i++;
+    }
+    return password.join('');
+}
+
 function partOne(input: string[]): string {
     return determinePassword(input[0]);
 }
 
-function partTwo(input: string[]): number {
-    return 0;
+function partTwo(input: string[]): string {
+    return determineEnhancedPassword(input[0]);
 }
 
 test(day, () => {
@@ -34,5 +51,6 @@ test(day, () => {
     expect(determinePassword('abc')).toBe('18f47a30');
     expect(partOne(getDayInput(day))).toBe('c6697b55');
 
-    expect(partTwo(getDayInput(day))).toBe(0);
+    expect(determineEnhancedPassword('abc')).toBe('05ace8e3');
+    expect(partTwo(getDayInput(day))).toBe('8c35d1ab');
 });
