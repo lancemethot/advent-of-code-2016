4e2cc6ccdbed9cb884cad6b0888731524da768e7
lancemethot <lance.methot@gmail.com>
2025-08-09 08:03:38 -0400

    day 10 part 1

---
+++ day10/day10.test.ts
@@ -0,0 +1,123 @@
+import { debug, getDayInput, getExampleInput } from 'advent-of-code-utils';
+
+const day = "day10";
+
+enum Destination {
+    BOT = 'bot',
+    BIN = 'output'
+};
+
+type BotRule = {
+    id: number;
+    low: {
+        type: Destination;
+        id: number;
+    };
+    high: {
+        type: Destination;
+        id: number;
+    };
+}
+
+type Bot = BotRule & {
+    id: number;
+    chips: number[];
+}
+
+function newBot(id: number): Bot {
+    return { id, chips: [], low: { id: -1, type: Destination.BIN }, high: { id: -1, type: Destination.BIN } };
+}
+
+function parseInput(input: string[]): Map<number, Bot> {
+    return input.reduce((acc, line) => {
+        let id: number = -1;
+        let bot: Bot | undefined = undefined;
+        let matches = line.match(/^value (\d+) goes to bot (\d+)$/);
+        if(matches !== null) {
+            id = Number.parseInt(matches[2]);
+            bot = acc.get(id) ?? newBot(id);
+            bot.chips.push(Number.parseInt(matches[1]));
+        } else {
+            matches = line.match(/^bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output) (\d+)$/);
+            if(matches !== null) {
+                id = Number.parseInt(matches[1]);
+                bot = acc.get(id) ?? newBot(id);
+
+                bot.low.id = Number.parseInt(matches[3]);
+                bot.low.type = matches[2] as Destination;
+
+                bot.high.id = Number.parseInt(matches[5]);
+                bot.high.type = matches[4] as Destination;
+            }
+        }
+        if(bot) acc.set(id, bot);
+        return acc;
+    }, new Map<number, Bot>());
+}
+
+function assign(bots: Map<number, number[]>, id: number, chip: number): number {
+    if(bots.has(id)) {
+        bots.get(id)!.push(chip);
+    } else {
+        bots.set(id, [ chip ]);
+    }
+    return bots.get(id)!.length;
+}
+
+function partOne(input: string[], low: number, high: number): number {
+
+    const bots = parseInput(input);
+    const bins: Map<number, number[]> = new Map<number, number[]>();
+
+    let checklist: number[] = Array.from(bots.keys());
+    let checkers: number[] = [];
+
+    while(checklist.length > 0) {
+        let id = checklist.shift();
+        let bot = bots.get(id!);
+        if(!bot) continue;
+
+        if(bot.chips.length === 2) {
+            bot.chips.sort((a, b) => a - b);
+            if(bot.chips[0] === low && bot.chips[1] === high) {
+                checkers.push(bot.id);
+            }
+
+            if(bot.low.type === Destination.BIN) {
+                let bin = bins.get(bot.low.id) || [];
+                bin.push(bot.chips[0]);
+                bins.set(bot.low.id, bin);
+            } else {
+                bots.get(bot.low.id)!.chips.push(bot.chips[0]);
+                checklist.push(bot.low.id);
+            }
+
+            if(bot.high.type === Destination.BIN) {
+                let bin = bins.get(bot.high.id) || [];
+                bin.push(bot.chips[1]);
+                bins.set(bot.high.id, bin);
+            } else {
+                bots.get(bot.high.id)!.chips.push(bot.chips[1]);
+                checklist.push(bot.high.id);
+            }
+
+            bots.get(bot.id)!.chips = [];
+        }
+    }
+
+    return checkers.pop() || -1;
+
+}
+
+function partTwo(input: string[]): number {
+    return 0;
+}
+
+test(day, () => {
+    debug(`[**${day}**] ${new Date()}\n\n`, day, false);
+
+    expect(partOne(getExampleInput(day), 2, 5)).toBe(2);
+    expect(partOne(getDayInput(day), 17, 61)).toBe(101);  
+
+    expect(partTwo(getDayInput(day))).toBe(0);
+});
