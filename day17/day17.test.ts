338df76d209a0f06ca050fb5f7a0ba223becdb52
lancemethot <lance.methot@gmail.com>
2025-08-10 11:50:20 -0400

    day 17 part 1

---
+++ day17/day17.test.ts
@@ -0,0 +1,79 @@
+import { debug, getDayInput, HeapItem, MinHeap } from 'advent-of-code-utils';
+import { createHash } from 'crypto';
+
+const day = 'day17';
+
+type Coord = {
+    x: number;
+    y: number;
+}
+
+type Path = HeapItem & {
+    position: Coord;
+    route: string;
+}
+
+function parseInput(input: string[]): string {
+    return input[0];
+}
+
+function md5(text: string): string {
+    return createHash('md5').update(text).digest('hex').toLocaleLowerCase();
+}
+
+function moves(position: Coord, passcode: string, route: string): { position: Coord, direction: string }[] {
+    const hash = md5(`${passcode}${route}`).substring(0, 4);
+    return [
+        { x: position.x - 1, y: position.y },
+        { x: position.x + 1, y: position.y },
+        { x: position.x,     y: position.y - 1 },
+        { x: position.x,     y: position.y + 1 }
+    ].reduce((acc, move, index) => {
+        if(move.x >= 0 && move.x < 4 && move.y >= 0 && move.y < 4) {
+            let c = hash[index];
+            if('bcdef'.includes(c)) acc.push({ position: move, direction: 'UDLR'[index] });
+        }
+        return acc;
+    }, [] as { position: Coord, direction: string }[]);
+}
+
+function dijkstra(passcode: string): string {
+    const heap: MinHeap<Path> = new MinHeap<Path>();
+
+    heap.insert({ size: 0, position: { x: 0, y: 0 }, route: '' });
+
+    while(heap.size() > 0) {
+        let next = heap.extractMin();
+
+        if(next.position.x === 3 && next.position.y === 3) {
+            return next.route;
+        }
+
+        moves(next.position, passcode, next.route).forEach(move => {
+            heap.insert({ size: next.size + 1, position: move.position, route: next.route + move.direction });
+        });
+
+    }
+
+    return '';
+}
+
+function partOne(input: string[]): string {
+    return dijkstra(parseInput(input));
+}
+
+function partTwo(input: string[]): string {
+    return '';
+}
+
+test(day, () => {
+    debug(`[**${day}**] ${new Date()}\n\n`, day, false);
+
+    expect(dijkstra('ihgpwlah')).toBe('DDRRRD');
+    expect(dijkstra('kglvqrro')).toBe('DDUDRLRRUDRD');
+    expect(dijkstra('ulqzkmiv')).toBe('DRURDRUDDLLDLUURRDULRLDUUDDDRR');
+
+    expect(partOne(getDayInput(day))).toBe('RDDRULDDRR');
+
+    expect(partTwo(getDayInput(day))).toBe('');
+});
